721b7eea2f251dda3fb69928c24cb760b09c0483
zhiyb <yz39g13@soton.ac.uk>
2015-08-03 18:13:07 +0100

    Mandelbrot algorithm optimise

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,41 +6,19 @@
 #define _cb(x) abs((x)*(x)*(x))	// absolute value of cube
 #define _cr(x) (unsigned char)(pow((x),1.0/3.0))	// cube root
 
-unsigned char RD(int i, int j)
-{
-	double a = 0, b = 0, c, d, n = 0;
-	while ((c = a * a) + (d = b * b) < 4 && n++ < 880) {
-		b = 2 * a * b + j * 8e-9 - .645411;
-		a = c - d + i * 8e-9 + .356888;
-	}
-	return 255 * pow((n - 80) / 800, 3.);
-}
-
-unsigned char GR(int i, int j)
-{
-	double a = 0, b = 0, c, d, n = 0;
-	while ((c = a * a) + (d = b * b) < 4 && n++ < 880) {
-		b = 2 * a * b + j * 8e-9 - .645411;
-		a = c - d + i * 8e-9 + .356888;
-	}
-	return 255 * pow((n - 80) / 800, .7);
-}
-
-unsigned char BL(int i, int j)
-{
-	double a = 0, b = 0, c, d, n = 0;
-	while ((c = a * a) + (d = b * b) < 4 && n++ < 880) {
-		b = 2 * a * b + j * 8e-9 - .645411;
-		a = c - d + i * 8e-9 + .356888;
-	}
-	return 255 * pow((n - 80) / 800, .5);
-}
-
 uint32_t pixel_write(int i, int j)
 {
 	uint32_t clr;
-	clr = RD(i, j) & 0xff;
-	clr |= (GR(i, j) & 0xff) << 8;
-	clr |= (BL(i, j) & 0xff) << 16;
+	double a = 0, b = 0, c, d, n = 0;
+	while ((c = a * a) + (d = b * b) < 4 && n++ < 880) {
+		b = 2 * a * b + j * 8e-9 - .645411;
+		a = c - d + i * 8e-9 + .356888;
+	}
+	unsigned char RD = 255 * pow((n - 80) / 800, 3.);
+	unsigned char GR = 255 * pow((n - 80) / 800, .7);
+	unsigned char BL = 255 * pow((n - 80) / 800, .5);
+	clr = (uint32_t)RD;
+	clr |= (uint32_t)GR << 8;
+	clr |= (uint32_t)BL << 16;
 	return clr;
 }
